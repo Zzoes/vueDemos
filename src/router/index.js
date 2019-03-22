9d409ba732b0856ddd55c5024c505399704c653c
Zzoes <34087249+Zzoes@users.noreply.github.com>
2019-03-22 11:59:07 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/page/home'
 import Item from '@/page/item'
+import Score from '@/page/score'
 
 Vue.use(Router)
 
@@ -15,6 +16,10 @@ export default new Router({
       path:'/item',
       name:'item',
       component:Item
+    },{
+      path:'/score',
+      name:'score',
+      component:Score
     }
   ],
   mode:'history'
